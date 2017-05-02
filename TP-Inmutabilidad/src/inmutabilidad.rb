9b65c259599a82c372cf256e05e335217c8af359
JuanAmarillo <juan.alb.amarillo@gmail.com>
2017-05-02 06:00:41 -0300

    Agrego crear instancias sin new y uso del initilize

--- TP-Inmutabilidad/src/inmutabilidad.rb
+++ TP-Inmutabilidad/src/inmutabilidad.rb
@@ -1,18 +1,36 @@
 #aca van los metodos de instancia
-module Comportamiento_case_class
+module Comportamiento_de_clase_case_class
 
-  def initialize
-    self.freeze
+  def inherited(subclass)
+    Object.send(:remove_const, subclass.name)
+    raise "no se puede Heredar de una case_class"
+  end
+
+  def attr_accessor(*symbols)
+    attr_reader(*symbols)
+    symbols.each do |symbol|
+      self.instance_variable_set("@#{symbol}",'inicializada')
+    end
+  end
+
+  def new(*args)
+    if(args.length == (self.instance_variables).length)
+      super(self.instance_variables.zip args)
+    else
+      raise "esto se va a descontrolaaaaaaarrrrrrr"
+    end
   end
 
 end
 
 #y aca los de clase
-module OtroModulo
+module Comportamiento_de_instancias_case_class
 
-  def inherited(subclass)
-      Object.send(:remove_const, subclass.name)
-      raise "no se puede Heredar de una case_class"
+  def initialize(args)
+    args.each do |variable,valor|
+      self.instance_variable_set(variable,valor)
+    end
+    self.freeze
   end
 
 end
@@ -34,7 +52,7 @@ module Entorno
     end
 
     def new_case_class#(&block)
-      Object.const_set(@nombre, (Class.new(@parent).extend Comportamiento_case_class))
+      Object.const_set(@nombre, (Class.new(@parent).extend Comportamiento_de_clase_case_class))
     end
 
 
@@ -47,8 +65,11 @@ module Entorno
     end
 
     def case_class (builder, &block)
-      una_case_class = builder.new_case_class.include OtroModulo
+      una_case_class = builder.new_case_class.include Comportamiento_de_instancias_case_class
       una_case_class.class_eval(&block)
+      Object.send(:define_method,una_case_class.name) do |*args|
+        una_case_class.new(*args)
+      end
     end
 
   end
@@ -60,8 +81,12 @@ end
 include Entorno
 
 case_class X do
+  attr_accessor :a, :b, :c
   def m1
     'm1'
+  end
+  def trampa
+    @a = 2
   end
 end
 
