275b3395ae2fc4e85bc6c8512620b65aca1ceb2b
JuanAmarillo <juan.alb.amarillo@gmail.com>
2017-06-07 00:46:32 -0300

    no se lo que estoy haciendo ajaj

--- TP-Funcional-Objetos/src/domain/Item.scala
+++ TP-Funcional-Objetos/src/domain/Item.scala
@@ -1,15 +1,10 @@
 package domain
 
-  class Item(
+ case class Item(
       danio: Int,
       incrementos:Option[Incrementos])
-      
-  case object Hacha extends Item(30,None)
-  case object Maza extends Item(100,None)
-  case object SistemaDeVuelo   extends Item(0,Some(Incrementos(velocidad = 1.3))) 
-  case object ComestibleHambre extends Item(0,Some(Incrementos(hambre    = 0.4)))
   
   case class Incrementos(
       velocidad : Double = 1,
       hambre: Double = 1
-      )
+   )
--- TP-Funcional-Objetos/src/domain/package.scala
+++ TP-Funcional-Objetos/src/domain/package.scala
@@ -2,6 +2,11 @@
 
 package object domain {
   
+  val Hacha   = Item(30,None)
+  val Maza    = Item(100,None)
+  val SistemaDeVuelo   = Item(0,Some(Incrementos(velocidad = 1.3)))
+  val ComestibleHambre = Item(0,Some(Incrementos(hambre    = 0.4)))
+  
   val Hipo    = Vikingo(item = SistemaDeVuelo)
   val Astrid  = Vikingo(item = Hacha)
   val Patan   = Vikingo(item = Maza)
