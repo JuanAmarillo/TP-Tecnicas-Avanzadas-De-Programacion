f86814b10251cdedcaf4b7aaadc74563c49a6d7d
JuanAmarillo <juan.alb.amarillo@gmail.com>
2017-06-07 00:43:20 -0300

    Valor a los items

--- TP-Funcional-Objetos/src/domain/Item.scala
+++ TP-Funcional-Objetos/src/domain/Item.scala
@@ -1,7 +1,15 @@
 package domain
 
-  sealed trait Item
-  case object Hacha extends Item
-  case object Maza extends Item
-  case object SistemaDeVuelo extends Item 
-  case object ComestibleHambre extends Item
+  class Item(
+      danio: Int,
+      incrementos:Option[Incrementos])
+      
+  case object Hacha extends Item(30,None)
+  case object Maza extends Item(100,None)
+  case object SistemaDeVuelo   extends Item(0,Some(Incrementos(velocidad = 1.3))) 
+  case object ComestibleHambre extends Item(0,Some(Incrementos(hambre    = 0.4)))
+  
+  case class Incrementos(
+      velocidad : Double = 1,
+      hambre: Double = 1
+      )
