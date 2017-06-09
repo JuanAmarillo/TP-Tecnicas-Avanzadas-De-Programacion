43dfb3d56026b6999fb6753ecf5b9cce2a6290a9
Achus619 <achus619@gmail.com>
2017-06-09 17:01:00 -0300

    Test de postas arreglado

--- TP-Funcional-Objetos/test/domain/PostasTest.scala
+++ TP-Funcional-Objetos/test/domain/PostasTest.scala
@@ -1,11 +1,14 @@
 package domain
 
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Assert.assertFalse
 import org.junit.Test
 
 class PostasTest {
   
+  val PatanDespuesDeCombate = Vikingo(60,1.0,50,0.1,item = Maza)
+     
   @Test
   def patanEsMejorQueAstridEnCombate(){
     assertTrue(Patan.esMejorQue(Astrid, Combate(0)))
@@ -13,7 +16,7 @@ class PostasTest {
   
   @Test
   def patanGanaLaPostaDeCombate(){
-    System.out.println(Combate(0).participar(participantesInvierno) )
+    assertTrue(Combate(0).participar(participantesInvierno).head.toString().equals(PatanDespuesDeCombate.toString()) )
   }
    
 }
