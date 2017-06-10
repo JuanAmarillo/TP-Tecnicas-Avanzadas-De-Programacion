23456077e8f6f839f77b23788af532829d52336a
JuanAmarillo <juan.alb.amarillo@gmail.com>
2017-06-10 02:41:34 -0300

    El torneo

--- TP-Funcional-Objetos/src/domain/Torneo.scala
+++ TP-Funcional-Objetos/src/domain/Torneo.scala
@@ -7,13 +7,31 @@ case object Eliminacion extends Regla
 
 case class Torneo(
 		  postas: List[Posta],
-		  participantes: List[Participante],
+		  participantesIniciales: List[Participante],
+		  reglas : Reglas
 		)
 {
-  def competir(regla : Regla) : Option[Participante] = {
-      regla match {
-      case Estandar        => 
-      case Eliminacion     =>
+  
+  def competir(regla : Regla) {
+    reglas.decidirGanador(jugarPostas())
+  }
+  
+  def jugarPostas() : List[Participante] = {
+    postas.foldLeft(participantesIniciales){(participantes,posta) =>
+     participantes match {
+        case participante  :: participantes => jugarPosta(participantes,posta)
+        case _                              => participantes
+      }
     }
   }
+  def jugarPosta(participantes: List[Participante],posta:Posta) : List[Participante] = {
+    val jugadores = posta.participar(participantes)
+    reglas.quienesAvanzan(jugadores)
+    
+  }
+}
+
+case class Reglas(){
+  def quienesAvanzan(participantes: List[Participante]) = ???
+  def decidirGanador(participantes: List[Participante]) = ???
 }
