b55e50170af92dfff87da88bf7aa19f7e9e24fa9
Achus619 <achus619@gmail.com>
2017-06-10 01:36:12 -0300

    Torneo v0.01

--- TP-Funcional-Objetos/src/domain/Torneo.scala
+++ TP-Funcional-Objetos/src/domain/Torneo.scala
@@ -1,7 +1,19 @@
 package domain 
-  
+ 
+sealed trait Regla
+
+case object Estandar extends Regla
+case object Eliminacion extends Regla
+
 case class Torneo(
 		  postas: List[Posta],
-		  participantes: List[Participante]
-	)
-  
+		  participantes: List[Participante],
+		)
+{
+  def competir(regla : Regla) : Option[Participante] = {
+      regla match {
+      case Estandar        => 
+      case Eliminacion     =>
+    }
+  }
+}
